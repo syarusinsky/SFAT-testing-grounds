eb4c5344154b739051a852bcaaf532391120d631
siike92 <siike92@hotmail.com>
2020-08-30 13:51:11 -0500

    Modified test code to use boot sector class

--- main.cpp
+++ main.cpp
@@ -6,75 +6,7 @@
 
 #include "Fat16Entry.hpp"
 #include "PartitionTable.hpp"
-
-// boot sector offsets and sizes
-#define BOOT_SEC_SIZE_IN_BYTES 				512
-#define BOOT_SEC_JUMP_OFFSET 				0x000
-#define BOOT_SEC_JUMP_SIZE 				3
-#define BOOT_SEC_OEM_NAME_OFFSET 			0x003
-#define BOOT_SEC_OEM_NAME_SIZE          		8
-#define BOOT_SEC_SECTOR_SIZE_OFFSET 			0x00B
-#define BOOT_SEC_SECTOR_SIZE_SIZE 			2
-#define BOOT_SEC_NUM_SECS_PER_CLUSTER_OFFSET 		0x00D
-#define BOOT_SEC_NUM_SECS_PER_CLUSTER_SIZE 		1
-#define BOOT_SEC_RESERVED_SECS_OFFSET 			0x00E
-#define BOOT_SEC_RESERVED_SECS_SIZE 			2
-#define BOOT_SEC_NUM_FATS_OFFSET 			0x010
-#define BOOT_SEC_NUM_FATS_SIZE 				1
-#define BOOT_SEC_NUM_DIRS_IN_ROOT_OFFSET 		0x011
-#define BOOT_SEC_NUM_DIRS_IN_ROOT_SIZE 			2
-#define BOOT_SEC_NUM_SECS_ON_DISK_LT_32MB_OFFSET 	0x013
-#define BOOT_SEC_NUM_SECS_ON_DISK_LT_32MB_SIZE 		2
-#define BOOT_SEC_MEDIA_DESCRIPTOR_OFFSET 		0x015
-#define BOOT_SEC_MEDIA_DESCRIPTOR_SIZE 			1
-#define BOOT_SEC_NUM_SECS_PER_FAT_OFFSET 		0x016
-#define BOOT_SEC_NUM_SECS_PER_FAT_SIZE 			2
-#define BOOT_SEC_NUM_SECS_PER_TRACK_OFFSET 		0x018
-#define BOOT_SEC_NUM_SECS_PER_TRACK_SIZE 		2
-#define BOOT_SEC_NUM_HEADS_OFFSET 			0x01A
-#define BOOT_SEC_NUM_HEADS_SIZE 			2
-#define BOOT_SEC_NUM_HIDDEN_SECS_OFFSET 		0x01C
-#define BOOT_SEC_NUM_HIDDEN_SECS_SIZE 			4
-#define BOOT_SEC_NUM_SECS_ON_DISK_GT_32MB_OFFSET 	0x020
-#define BOOT_SEC_NUM_SECS_ON_DISK_GT_32MB_SIZE 		4
-#define BOOT_SEC_NUM_SECS_PER_FAT_FAT32_OFFSET 		0x024
-#define BOOT_SEC_NUM_SECS_PER_FAT_FAT32_SIZE 		4
-#define BOOT_SEC_FAT_MIRR_ACTIVE_FAT_FAT32_OFFSET 	0x028
-#define BOOT_SEC_FAT_MIRR_ACTIVE_FAT_FAT32_SIZE 	2
-#define BOOT_SEC_FILE_SYSTEM_VER_FAT32_OFFSET 		0x02A
-#define BOOT_SEC_FILE_SYSTEM_VER_FAT32_SIZE 		2
-#define BOOT_SEC_CLUSTER_NUM_FOR_ROOT_FAT32_OFFSET 	0x02C
-#define BOOT_SEC_CLUSTER_NUM_FOR_ROOT_FAT32_SIZE 	4
-#define BOOT_SEC_FSINFO_SEC_NUM_FAT32_OFFSET 		0x030
-#define BOOT_SEC_FSINFO_SEC_NUM_FAT32_SIZE 		2
-#define BOOT_SEC_BACKUP_SEC_NUM_FAT32_OFFSET 		0x032
-#define BOOT_SEC_BACKUP_SEC_NUM_FAT32_SIZE 		2
-#define BOOT_SEC_RESERVED_FAT32_OFFSET 			0x034
-#define BOOT_SEC_RESERVED_FAT32_SIZE 			12
-#define BOOT_SEC_DRIVE_NUMBER_FAT32_OFFSET 		0x040
-#define BOOT_SEC_DRIVE_NUMBER_FAT16_OFFSET 		0x024
-#define BOOT_SEC_DRIVE_NUMBER_SIZE 			1
-#define BOOT_SEC_CURRENT_HEAD_FAT32_OFFSET 		0x041
-#define BOOT_SEC_CURRENT_HEAD_FAT16_OFFSET 		0x025
-#define BOOT_SEC_CURRENT_HEAD_SIZE 			1
-#define BOOT_SEC_BOOT_SIGNATURE_FAT32_OFFSET 		0x042
-#define BOOT_SEC_BOOT_SIGNATURE_FAT16_OFFSET 		0x026
-#define BOOT_SEC_BOOT_SIGNATURE_SIZE 			1
-#define BOOT_SEC_VOLUME_ID_FAT32_OFFSET 		0x043
-#define BOOT_SEC_VOLUME_ID_FAT16_OFFSET 		0x027
-#define BOOT_SEC_VOLUME_ID_SIZE 			4
-#define BOOT_SEC_VOLUME_LABEL_FAT32_OFFSET 		0x047
-#define BOOT_SEC_VOLUME_LABEL_FAT16_OFFSET 		0x02B
-#define BOOT_SEC_VOLUME_LABEL_SIZE 			11
-#define BOOT_SEC_FILE_SYS_TYPE_FAT32_OFFSET 		0x052
-#define BOOT_SEC_FILE_SYS_TYPE_FAT16_OFFSET 		0x036
-#define BOOT_SEC_FILE_SYS_TYPE_SIZE 			8
-#define BOOT_SEC_BOOT_CODE_FAT32_OFFSET 		0x08A
-#define BOOT_SEC_BOOT_CODE_FAT32_SIZE 			372
-#define BOOT_SEC_BOOT_CODE_FAT16_OFFSET 		0x03E
-#define BOOT_SEC_BOOT_CODE_FAT16_SIZE 			448
-#define BOOT_SEC_BOOT_SECTOR_SIGNATURE_OFFSET 		0x1FE
-#define BOOT_SEC_BOOT_SECTOR_SIGNATURE_SIZE 		2
+#include "BootSector.hpp"
 
 // parition table offsets
 #define PARTITION_TABLE_1_OFFSET 0x1BE
@@ -144,258 +76,35 @@ void printFat16Entry (const Fat16Entry& entry)
 	std::cout << "Entry size in bytes : " << std::to_string( entry.getFileSizeInBytes() ) << std::endl;
 }
 
-class BootSector
+void printBootSector (const BootSector& bootSector)
 {
-	public:
-		BootSector (uint8_t* offset, const PartitionType& partitionType = PartitionType::EMPTY) :
-			m_PartitionType( partitionType ),
-			m_JumpInstruction( (offset[2] << 16) | (offset[1] << 8) | (offset[0]) ),
-			m_OemName{ 0 }, // we'll do this in a loop in the constructor
-			m_SectorSizeInBytes( (offset[BOOT_SEC_SECTOR_SIZE_OFFSET + 1] << 8) | (offset[BOOT_SEC_SECTOR_SIZE_OFFSET]) ),
-			m_NumSectorsPerCluster( offset[BOOT_SEC_NUM_SECS_PER_CLUSTER_OFFSET] ),
-			m_NumReservedSectors( (offset[BOOT_SEC_RESERVED_SECS_OFFSET + 1] << 8) | offset[BOOT_SEC_RESERVED_SECS_OFFSET] ),
-			m_NumFats( offset[BOOT_SEC_NUM_FATS_OFFSET] ),
-			m_NumDirEntriesInRoot( (offset[BOOT_SEC_NUM_DIRS_IN_ROOT_OFFSET + 1] << 8) | offset[BOOT_SEC_NUM_DIRS_IN_ROOT_OFFSET] ),
-			m_NumSectorsOnDisk( (offset[BOOT_SEC_NUM_SECS_ON_DISK_LT_32MB_OFFSET + 1] << 8) // this may be zero, so we use logic in the
-						| offset[BOOT_SEC_NUM_SECS_ON_DISK_LT_32MB_OFFSET] ), 	// constructor to find true size
-			m_MediaDescriptor( offset[BOOT_SEC_MEDIA_DESCRIPTOR_OFFSET] ),
-			m_NumSectorsPerFat( (offset[BOOT_SEC_NUM_SECS_PER_FAT_OFFSET + 1] << 8) | offset[BOOT_SEC_NUM_SECS_PER_FAT_OFFSET] ),
-			m_NumSectorsPerTrack( (offset[BOOT_SEC_NUM_SECS_PER_TRACK_OFFSET + 1] << 8) | offset[BOOT_SEC_NUM_SECS_PER_TRACK_OFFSET] ),
-			m_NumHeads( (offset[BOOT_SEC_NUM_HEADS_OFFSET + 1] << 8) | offset[BOOT_SEC_NUM_HEADS_OFFSET] ),
-			m_NumHiddenSectors( (offset[BOOT_SEC_NUM_HIDDEN_SECS_OFFSET + 3] << 24)
-						| (offset[BOOT_SEC_NUM_HIDDEN_SECS_OFFSET + 2] << 16)
-						| (offset[BOOT_SEC_NUM_HIDDEN_SECS_OFFSET + 1] << 8)
-						| offset[BOOT_SEC_NUM_HIDDEN_SECS_OFFSET] ),
-			m_DriveNum( offset[BOOT_SEC_DRIVE_NUMBER_FAT16_OFFSET] ),
-			m_CurrentHead( offset[BOOT_SEC_CURRENT_HEAD_FAT16_OFFSET] ),
-			m_BootSignature( offset[BOOT_SEC_BOOT_SIGNATURE_FAT16_OFFSET] ),
-			m_VolumeID( (offset[BOOT_SEC_VOLUME_ID_FAT16_OFFSET + 3] << 24)
-						| (offset[BOOT_SEC_VOLUME_ID_FAT16_OFFSET + 2] << 16)
-						| (offset[BOOT_SEC_VOLUME_ID_FAT16_OFFSET + 1] << 8)
-						| offset[BOOT_SEC_VOLUME_ID_FAT16_OFFSET] ),
-			m_VolumeLabel{ 0 },
-			m_FileSystemType{ 0 },
-			// ignoring boot code
-			m_BootSectorSignature( (offset[BOOT_SEC_BOOT_SECTOR_SIGNATURE_OFFSET + 1] << 8)
-						| offset[BOOT_SEC_BOOT_SECTOR_SIGNATURE_OFFSET] ),
-			m_NumSectorsForFat( (offset[BOOT_SEC_NUM_SECS_PER_FAT_FAT32_OFFSET + 3] << 24)
-						| (offset[BOOT_SEC_NUM_SECS_PER_FAT_FAT32_OFFSET + 2] << 16)
-						| (offset[BOOT_SEC_NUM_SECS_PER_FAT_FAT32_OFFSET + 1] << 8)
-						| offset[BOOT_SEC_NUM_SECS_PER_FAT_FAT32_OFFSET] ),
-			m_FlagsForFatMirroringAndActiveFat( (offset[BOOT_SEC_FAT_MIRR_ACTIVE_FAT_FAT32_OFFSET + 1] << 8)
-								| offset[BOOT_SEC_FAT_MIRR_ACTIVE_FAT_FAT32_OFFSET] ),
-			m_FileSystemVersionNum( (offset[BOOT_SEC_FILE_SYSTEM_VER_FAT32_OFFSET + 1] << 8)
-						| offset[BOOT_SEC_FILE_SYSTEM_VER_FAT32_OFFSET] ),
-			m_ClusterNumForRoot( (offset[BOOT_SEC_CLUSTER_NUM_FOR_ROOT_FAT32_OFFSET + 3] << 24)
-						| (offset[BOOT_SEC_CLUSTER_NUM_FOR_ROOT_FAT32_OFFSET + 2] << 16)
-						| (offset[BOOT_SEC_CLUSTER_NUM_FOR_ROOT_FAT32_OFFSET + 1] << 8)
-						| offset[BOOT_SEC_CLUSTER_NUM_FOR_ROOT_FAT32_OFFSET] ),
-			m_FSInfoSectorNum( (offset[BOOT_SEC_FSINFO_SEC_NUM_FAT32_OFFSET + 1] << 8)
-						| offset[BOOT_SEC_FSINFO_SEC_NUM_FAT32_OFFSET] ),
-			m_BackupSectorNum( (offset[BOOT_SEC_BACKUP_SEC_NUM_FAT32_OFFSET + 1] << 8)
-						| offset[BOOT_SEC_BACKUP_SEC_NUM_FAT32_OFFSET] )
-		{
-			// get the OEM name
-			for ( unsigned int character = 0; character < BOOT_SEC_OEM_NAME_SIZE; character++ )
-			{
-				m_OemName[character] = offset[BOOT_SEC_OEM_NAME_OFFSET + character];
-			}
-
-			// if num sectors is 0, the partition is likely > 32MB and we need to check other offset
-			if ( m_NumSectorsOnDisk == 0 )
-			{
-				m_NumSectorsOnDisk = (offset[BOOT_SEC_NUM_SECS_ON_DISK_GT_32MB_OFFSET + 3] << 24)
-							| (offset[BOOT_SEC_NUM_SECS_ON_DISK_GT_32MB_OFFSET + 2] << 16)
-							| (offset[BOOT_SEC_NUM_SECS_ON_DISK_GT_32MB_OFFSET + 1] << 8)
-							| offset[BOOT_SEC_NUM_SECS_ON_DISK_GT_32MB_OFFSET];
-			}
-
-			// if the file system type isn't FAT16 or FAT12, we need to use FAT32 offsets
-			if ( partitionType == PartitionType::FAT32_LTOREQ_2GB || partitionType == PartitionType::FAT32_LBA )
-			{
-				m_DriveNum = offset[BOOT_SEC_DRIVE_NUMBER_FAT32_OFFSET];
-				m_CurrentHead = offset[BOOT_SEC_CURRENT_HEAD_FAT32_OFFSET];
-				m_BootSignature = offset[BOOT_SEC_BOOT_SIGNATURE_FAT32_OFFSET];
-				m_VolumeID = (offset[BOOT_SEC_VOLUME_ID_FAT32_OFFSET + 3] << 24)
-						| (offset[BOOT_SEC_VOLUME_ID_FAT32_OFFSET + 2] << 16)
-						| (offset[BOOT_SEC_VOLUME_ID_FAT32_OFFSET + 1] << 8)
-						| offset[BOOT_SEC_VOLUME_ID_FAT32_OFFSET];
-
-				// get the file system type name from the FAT32 offset
-				for ( unsigned int character = 0; character < BOOT_SEC_FILE_SYS_TYPE_SIZE; character++ )
-				{
-					m_FileSystemType[character] = offset[BOOT_SEC_FILE_SYS_TYPE_FAT32_OFFSET + character];
-				}
-
-				// get the volume label from the FAT32 offset
-				for ( unsigned int character = 0; character < BOOT_SEC_VOLUME_LABEL_SIZE; character++ )
-				{
-					m_VolumeLabel[character] = offset[BOOT_SEC_VOLUME_LABEL_FAT32_OFFSET + character];
-				}
-			}
-			else // a FAT16 or FAT12 file system
-			{
-
-				// get the file system type name from the FAT16/FAT12 offset
-				for ( unsigned int character = 0; character < BOOT_SEC_FILE_SYS_TYPE_SIZE; character++ )
-				{
-					m_FileSystemType[character] = offset[BOOT_SEC_FILE_SYS_TYPE_FAT16_OFFSET + character];
-				}
-
-				// get the volume label from the FAT16/FAT12 offset
-				for ( unsigned int character = 0; character < BOOT_SEC_VOLUME_LABEL_SIZE; character++ )
-				{
-					m_VolumeLabel[character] = offset[BOOT_SEC_VOLUME_LABEL_FAT16_OFFSET + character];
-				}
-			}
-		}
-
-		~BootSector() {}
-
-		PartitionType getPartitionType() const { return m_PartitionType; }
-
-		bool volumeIdLabelAndFSTypeAreValid() const
-		{
-			if ( m_BootSignature == 0x29 )
-			{
-				return true;
-			}
-
-			return false;
-		}
-
-		bool bootSectorSignatureIsValid() const
-		{
-			if ( m_BootSectorSignature == 0xAA55 )
-			{
-				return true;
-			}
-
-			return false;
-		}
-
-		uint32_t getJumpInstruction() const { return m_JumpInstruction; }
-
-		const char* getOemName() const { return m_OemName; }
-
-		uint16_t getSectorSizeInBytes() const { return m_SectorSizeInBytes; }
-
-		uint8_t getNumSectorsPerCluster() const { return m_NumSectorsPerCluster; }
-
-		uint16_t getNumReservedSectors() const { return m_NumReservedSectors; }
-
-		uint8_t getNumFats() const { return m_NumFats; }
-
-		uint16_t getNumDirectoryEntriesInRoot() const { return m_NumDirEntriesInRoot; }
-
-		uint32_t getNumSectorsOnDisk() const { return m_NumSectorsOnDisk; }
-
-		uint8_t getMediaDescriptor() const { return m_MediaDescriptor; }
-
-		uint16_t getNumSectorsPerFat() const
-		{
-			if ( m_PartitionType == PartitionType::FAT32_LTOREQ_2GB
-					|| m_PartitionType == PartitionType::FAT32_LBA )
-			{
-				return m_NumSectorsForFat;
-			}
-
-			return m_NumSectorsPerFat;
-		}
-
-		uint16_t getNumSectorsPerTrack() const { return m_NumSectorsPerTrack; }
-
-		uint16_t getNumHeads() const { return m_NumHeads; }
-
-		uint32_t getNumHiddenSectors() const { return m_NumHiddenSectors; }
-
-		uint8_t getDriveNum() const { return m_DriveNum; }
-
-		uint8_t getCurrentHead() const { return m_CurrentHead; }
-
-		uint8_t getBootSignature() const { return m_BootSignature; }
-
-		uint32_t getVolumeID() const { return m_VolumeID; }
-
-		const char* getVolumeLabel() const { return m_VolumeLabel; }
-
-		const char* getFileSystemType() const { return m_FileSystemType; }
-
-		uint16_t getBootSectorSignature() const { return m_BootSectorSignature; }
-
-		uint16_t getFlagsForFatMirroringAndActiveFat() const { return m_FlagsForFatMirroringAndActiveFat; }
-
-		uint16_t getFileSystemVersionNum() const { return m_FileSystemVersionNum; }
-
-		uint32_t getClusterNumForRoot() const { return m_ClusterNumForRoot; }
-
-		uint16_t getFSInfoSectorNum() const { return m_FSInfoSectorNum; }
-
-		uint16_t getBackupSectorNum() const { return m_BackupSectorNum; }
-
-		void print() const
-		{
-			std::cout << "Jump instruction : " << std::to_string( m_JumpInstruction ) << std::endl;
-			std::cout << "OEM name : " << m_OemName << std::endl;
-			std::cout << "Sector size in bytes : " << std::to_string( m_SectorSizeInBytes ) << std::endl;
-			std::cout << "Number of sectors per cluster : " << std::to_string( m_NumSectorsPerCluster ) << std::endl;
-			std::cout << "Number of reserved sectors : " << std::to_string( m_NumReservedSectors ) << std::endl;
-			std::cout << "Number of FATs : " << std::to_string( m_NumFats ) << std::endl;
-			std::cout << "Number of directory entries in root (if FAT16) : " << std::to_string( m_NumDirEntriesInRoot ) << std::endl;
-			std::cout << "Number of sectors on disk : " << std::to_string( m_NumSectorsOnDisk ) << std::endl;
-			std::cout << "Media descriptor : " << std::to_string( m_MediaDescriptor ) << std::endl;
-			std::cout << "Number of sectors per FAT (if FAT16) : " << std::to_string( m_NumSectorsPerFat ) << std::endl;
-			std::cout << "Number of sectors per track : " << std::to_string( m_NumSectorsPerTrack ) << std::endl;
-			std::cout << "Number of heads : " << std::to_string( m_NumHeads ) << std::endl;
-			std::cout << "Number of hidden sectors : " << std::to_string( m_NumHiddenSectors ) << std::endl;
-			std::cout << "Drive number : " << std::to_string( m_DriveNum ) << std::endl;
-			std::cout << "Current head : " << std::to_string( m_CurrentHead ) << std::endl;
-			std::cout << "Boot signature : " << std::to_string( m_BootSignature ) << std::endl;
-			std::cout << "Volume ID : " << std::to_string( m_VolumeID ) << std::endl;
-			std::cout << "Volume label : " << m_VolumeLabel << std::endl;
-			std::cout << "File system name : " << m_FileSystemType << std::endl;
-			std::cout << "Boot sector signature : " << std::to_string( m_BootSectorSignature ) << std::endl;
-			std::cout << "(if FAT32) Number of sectors per FAT : " << std::to_string( m_NumSectorsForFat ) << std::endl;
-			std::cout << "(if FAT32) Flags for mirroring and active : " << std::to_string( m_FlagsForFatMirroringAndActiveFat ) << std::endl;
-			std::cout << "(if FAT32) File system version number : " << std::to_string( m_FileSystemVersionNum ) << std::endl;
-			std::cout << "(if FAT32) Cluster number for root : " << std::to_string( m_ClusterNumForRoot ) << std::endl;
-			std::cout << "(if FAT32) FSInfo sector number : " << std::to_string( m_FSInfoSectorNum ) << std::endl;
-			std::cout << "(if FAT32) Backup boot sector sector number : " << std::to_string( m_BackupSectorNum ) << std::endl;
-		}
-
-	private:
-		PartitionType 	m_PartitionType;
-
-		uint32_t 	m_JumpInstruction; // never going to use this, but whatever
-		char 	 	m_OemName[BOOT_SEC_OEM_NAME_SIZE];
-		uint16_t 	m_SectorSizeInBytes;
-		uint8_t 	m_NumSectorsPerCluster;
-		uint16_t 	m_NumReservedSectors; // including boot sector
-		uint8_t 	m_NumFats;
-		uint16_t 	m_NumDirEntriesInRoot; // NA for FAT32
-		uint32_t 	m_NumSectorsOnDisk;
-		uint8_t 	m_MediaDescriptor;
-		uint16_t 	m_NumSectorsPerFat; // NA for FAT32
-		uint16_t 	m_NumSectorsPerTrack; // CHS addressing
-		uint16_t 	m_NumHeads; // CHS addressing
-		uint32_t 	m_NumHiddenSectors; // sectors before the boot sector
-		uint8_t 	m_DriveNum;
-		uint8_t 	m_CurrentHead;
-		uint8_t 	m_BootSignature; // must be 0x29 for next 3 fields to be valid
-		uint32_t 	m_VolumeID;
-		char 		m_VolumeLabel[BOOT_SEC_VOLUME_LABEL_SIZE];
-		char 		m_FileSystemType[BOOT_SEC_FILE_SYS_TYPE_SIZE];
-		// uint8_t 	m_BootCode[448]; // not currently using this, as we don't actually plan to boot from SD card
-		uint16_t 	m_BootSectorSignature; // must be 0xAA55
-
-		// FAT32 only!
-		uint32_t 	m_NumSectorsForFat;
-		uint16_t 	m_FlagsForFatMirroringAndActiveFat;
-		uint16_t 	m_FileSystemVersionNum;
-		uint32_t 	m_ClusterNumForRoot;
-		uint16_t 	m_FSInfoSectorNum;
-		uint16_t 	m_BackupSectorNum;
-};
+	std::cout << "Jump instruction : " << std::to_string( bootSector.getJumpInstruction() ) << std::endl;
+	std::cout << "OEM name : " << bootSector.getOemName() << std::endl;
+	std::cout << "Sector size in bytes : " << std::to_string( bootSector.getSectorSizeInBytes() ) << std::endl;
+	std::cout << "Number of sectors per cluster : " << std::to_string( bootSector.getNumSectorsPerCluster() ) << std::endl;
+	std::cout << "Number of reserved sectors : " << std::to_string( bootSector.getNumReservedSectors() ) << std::endl;
+	std::cout << "Number of FATs : " << std::to_string( bootSector.getNumFats() ) << std::endl;
+	std::cout << "Number of directory entries in root (if FAT16) : " << std::to_string( bootSector.getNumDirectoryEntriesInRoot() ) << std::endl;
+	std::cout << "Number of sectors on disk : " << std::to_string( bootSector.getNumSectorsOnDisk() ) << std::endl;
+	std::cout << "Media descriptor : " << std::to_string( bootSector.getMediaDescriptor() ) << std::endl;
+	std::cout << "Number of sectors per FAT (if FAT16) : " << std::to_string( bootSector.getNumSectorsPerFat() ) << std::endl;
+	std::cout << "Number of sectors per track : " << std::to_string( bootSector.getNumSectorsPerTrack() ) << std::endl;
+	std::cout << "Number of heads : " << std::to_string( bootSector.getNumHeads() ) << std::endl;
+	std::cout << "Number of hidden sectors : " << std::to_string( bootSector.getNumHiddenSectors() ) << std::endl;
+	std::cout << "Drive number : " << std::to_string( bootSector.getDriveNum() ) << std::endl;
+	std::cout << "Current head : " << std::to_string( bootSector.getCurrentHead() ) << std::endl;
+	std::cout << "Boot signature : " << std::to_string( bootSector.getBootSignature() ) << std::endl;
+	std::cout << "Volume ID : " << std::to_string( bootSector.getVolumeID() ) << std::endl;
+	std::cout << "Volume label : " << bootSector.getVolumeLabel() << std::endl;
+	std::cout << "File system name : " << bootSector.getFileSystemType() << std::endl;
+	std::cout << "Boot sector signature : " << std::to_string( bootSector.getBootSectorSignature() ) << std::endl;
+	std::cout << "(if FAT32) Number of sectors per FAT : " << std::to_string( bootSector.getNumSectorsPerFat() ) << std::endl;
+	std::cout << "(if FAT32) Flags for mirroring and active : " << std::to_string( bootSector.getFlagsForFatMirroringAndActiveFat() ) << std::endl;
+	std::cout << "(if FAT32) File system version number : " << std::to_string( bootSector.getFileSystemVersionNum() ) << std::endl;
+	std::cout << "(if FAT32) Cluster number for root : " << std::to_string( bootSector.getClusterNumForRoot() ) << std::endl;
+	std::cout << "(if FAT32) FSInfo sector number : " << std::to_string( bootSector.getFSInfoSectorNum() ) << std::endl;
+	std::cout << "(if FAT32) Backup boot sector sector number : " << std::to_string( bootSector.getBackupSectorNum() ) << std::endl;
+}
 
 void printPartitionTable (const PartitionTable& partitionTable)
 {
@@ -442,7 +151,7 @@ int main()
 		fread( bsBuffer, sizeof(uint8_t), BOOT_SEC_SIZE_IN_BYTES, sdCardImage );
 
 		BootSector bootSector( bsBuffer );
-		bootSector.print();
+		printBootSector( bootSector );
 
 		// navigate to root directory and print entries
 		uint8_t entryBuffer[FAT16_ENTRY_SIZE * bootSector.getNumDirectoryEntriesInRoot()];
--- makeAndRun.sh
+++ makeAndRun.sh
@@ -1,2 +1,2 @@
-g++ -o main main.cpp SFAT/src/Fat16Entry.cpp SFAT/src/PartitionTable.cpp -ISFAT/include
+g++ -o main main.cpp SFAT/src/Fat16Entry.cpp SFAT/src/PartitionTable.cpp SFAT/src/BootSector.cpp -ISFAT/include
 ./main
